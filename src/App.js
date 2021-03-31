4e5c70e207d8f0cbe86a9673d422d3dea0ab996e
Grégory D'Angelo <dangelo.gregory@gmail.com>
2021-03-31 11:21:33 +0200

    chore: remove unnecessary boilerplate code + add icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Lightning } from './icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="h-screen flex flex-col justify-center items-center space-y-9 p-6">
+      <div className="text-center space-y-3">
+        <h1 className="text-4xl font-semibold tracking-wide flex flex-col sm:flex-row items-center justify-center">
+          <Lightning className="w-12 h-12 mb-1 sm:mb-0 sm:mr-1 flex-shrink-0 animate-pulse" />{' '}
+          Jumpstart React
+        </h1>
+        <p className="text-xl">
+          Now it's your turn. Build and style this application.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="text-xl text-center space-y-2 px-4 sm:px-8 py-4 bg-gray-100 rounded-lg max-w-lg">
+        <p>
+          If you need help, feel free to go to the{' '}
+          <a
+            href="http://discord.gg/HTQvwxA"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-600 hover:underline transition-colors"
+          >
+            Discord server
+          </a>{' '}
+          and ask the community.
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/icons/Check.js
@@ -0,0 +1,18 @@
+const Check = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M5 13l4 4L19 7"
+    />
+  </svg>
+);
+
+export default Check;
